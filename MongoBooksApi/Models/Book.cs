619f1e38a766ef0ca78516c7e6a1f1718d0cf913
LZ <liuzhellll@hotmail.com>
2019-12-30 16:06:57 +0800

    添加项目文件。

---
+++ MongoBooksApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace MongoBooksApi.Models
+{
+    public class Book
+    {
+        [BsonId] // 指定为文档的主键
+        [BsonRepresentation(BsonType.ObjectId)] // 将参数作为类型 string 而非 ObjectId 结构传递。 Mongo 处理从 string 到 ObjectId 的转换
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // Name 的属性值表示 MongoDB 集合中的属性名称
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ MongoBooksApi/Services/BookService.cs
@@ -0,0 +1,44 @@
+﻿using MongoBooksApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoBooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            
+            var database = client.GetDatabase(settings.DatabaseName); // Mongo 数据库
+            
+            // GetCollection<TDocument>(collection) 
+            // TDocument 表示存储在集合中的 CLR 对象类型
+            // collection 表示集合名称
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        // 返回集合中与提供的搜索条件匹配的所有文档
+        public List<Book> Get() => _books.Find(book => true).ToList();
+
+        public Book Get(string id) => _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        // 插入提供的对象作为集合中的新文档
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        // 将与提供的搜索条件匹配的单个文档替换为提供的对象
+        public void Update(string id, Book bookIn) => _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        // 删除与提供的搜索条件匹配的单个文档
+        public void Remove(Book bookIn) => _books.DeleteOne(book => book.Id == bookIn.Id);
+        // 删除与提供的搜索条件匹配的单个文档
+        public void Remove(string id) => _books.DeleteOne(book => book.Id == id);
+    }
+}
